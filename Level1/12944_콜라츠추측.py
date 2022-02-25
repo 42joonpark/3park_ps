9c706603ba1c3de356392260ad93587ca0970864
Donghyeon Park <donpark@c1r8s3.42seoul.kr>
2022-02-25 13:17:52 +0900

    20220225 13:17

---
+++ Level1/12944_콜라츠추측.py
@@ -0,0 +1,14 @@
+def solution(num):
+	answer = 0
+	while (True):
+		if (num == 1):
+			break
+		if (num % 2 == 0):
+			num /= 2
+		else:
+			num = num * 3 + 1
+		answer += 1
+		if (answer > 500):
+			answer = -1
+			break
+	return answer
