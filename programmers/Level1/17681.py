8f96cf7b6d76c5524cf575183a5219689bcb100a
padohy <padohy@gmail.com>
2022-02-26 00:04:42 +0900

    1차 비밀지도

--- programmers/Level1/17681.py
+++ programmers/Level1/17681.py
@@ -1,4 +1,4 @@
-
+# [1차] 비밀지도
 def changeBinary(num, n):
 	lst = []
 	while num != 0:
@@ -10,23 +10,16 @@ def changeBinary(num, n):
 	lst.reverse()
 	return (lst)
 
-# print(changeBinary(9, 5))
-
 def solution(n, arr1, arr2):
 	answer = []
 	arr = []
 	brr = []
-	crr = [[0]*n] * n
+	crr = [[0 for i in range(n)] for j in range(n)]		# crr = [[0]*n] * n  (python에서 배열 생성시 동작 방식의 차이가 있다.)
 	print(crr)
 	for a in arr1:
 		arr.append(changeBinary(a, n))
-	print(arr)
-
-
 	for b in arr2:
 		brr.append(changeBinary(b, n))
-	print(brr)
-
 	for i in range(0, n):
 		for j in range(0, n):
 			k = arr[i][j] + brr[i][j]
@@ -36,18 +29,8 @@ def solution(n, arr1, arr2):
 			else:
 				print(i,j)
 				crr[i][j] = '#'
-			print(f"k: {k} | i, j: {i}, {j} | crr[{i}][{j}]: ",crr[i][j])
-
-	print("")
-	print(crr)
-
 	for i in range(0, n):
 		answer.append(''.join(crr[i]))
-
-
-	# result = [ i + j for i in arr[0] for j in brr[0] ]
-	# print(result)
-
 	return answer
 
 print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
